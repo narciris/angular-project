6c90842c0da22656b28f50fd17c26b67fbe740ca
Nar Mena <narciris.tech@gmail.com>
2025-04-02 19:33:43 -0500

    create component

---
+++ src/app/user-component/user-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-component',
+  imports: [],
+  templateUrl: './user-component.component.html',
+  styleUrl: './user-component.component.css'
+})
+export class UserComponentComponent {
+
+}
